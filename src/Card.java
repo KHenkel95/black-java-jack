8fafc640c5ab896c5681c225ee648862c354eebb
Ken Henkel <kenhenkel95@gmail.com>
2022-06-07 14:56:59 -0400

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+    private String cardSuit;
+    private String cardFace;
+    private int cardValue;
+
+    public Card(String cardSuit, String cardFace, int cardValue){
+        this.cardSuit = cardSuit;
+        this.cardFace = cardFace;
+        this.cardValue = cardValue;
+    }
+
+    
+}
